5fbb5660551fd15e7fb137ce77094b688b1b80d7
Eli Almario <ee.almario@gmail.com>
2020-12-26 11:16:56 +0800

    Add random food position

--- js/constants.js
+++ js/constants.js
@@ -3,3 +3,6 @@ export const SNAKE_SPEED = 5; //how many times the snake moves per second
 
 // FOOD
 export const EXPANSION_RATE = 1; //how much the snake would grow if it eats the food
+
+// GRID SIZE
+export const GRID_SIZE = 21;
---
+++ js/grid.js
@@ -0,0 +1,8 @@
+import { GRID_SIZE } from './constants.js';
+
+export function randomGridPosition() {
+  return {
+    x: Math.floor(Math.random() * GRID_SIZE) + 1,
+    y: Math.floor(Math.random() * GRID_SIZE) + 1
+  }
+}
