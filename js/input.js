a1f69b879d5cbfe40355d4eeacee013c2380c084
Eli Almario <ee.almario@gmail.com>
2020-12-26 22:25:13 +0800

    Updates

---
+++ js/input.js
@@ -0,0 +1,57 @@
+const arrowUp = document.getElementById('arrowUp');
+const arrowLeft = document.getElementById('arrowLeft');
+const arrowRight = document.getElementById('arrowRight');
+const arrowDown = document.getElementById('arrowDown');
+
+let inputDirection = { x: 0, y: 0 };
+let lastInputDirection = { x: 0, y: 0};
+
+window.addEventListener('keydown', handleDirection);
+window.addEventListener('click', handleMobileDirection);
+
+function handleDirection(e) {
+  switch (e.keyCode) {
+    case 38:
+      if (lastInputDirection.y !== 0) break;
+      inputDirection = { x: 0, y: -1 };
+      break;
+    case 40:
+      if (lastInputDirection.y !== 0) break;
+      inputDirection = { x: 0, y: 1 };
+      break;
+    case 37:
+      if (lastInputDirection.x !== 0) break;
+      inputDirection = { x: -1, y: 0 };
+      break;
+    case 39:
+      if (lastInputDirection.x !== 0) break;
+      inputDirection = { x: 1, y: 0 };
+      break;
+  }
+}
+
+function handleMobileDirection(e) {
+  switch(e.target) {
+    case arrowUp:
+      if (lastInputDirection.y !== 0) break;
+      inputDirection = { x: 0, y: -1 };
+      break;
+    case arrowDown:
+      if (lastInputDirection.y !== 0) break;
+      inputDirection = { x: 0, y: 1 };
+      break;
+    case arrowLeft:
+      if (lastInputDirection.x !== 0) break;
+      inputDirection = { x: -1, y: 0 };
+      break;
+    case arrowRight:
+      if (lastInputDirection.x !== 0) break;
+      inputDirection = { x: 1, y: 0 };
+      break;
+  }
+}
+
+export function getInputDirection () {
+  lastInputDirection = inputDirection;
+  return inputDirection;
+}
