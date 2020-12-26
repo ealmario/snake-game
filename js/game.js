a7683c27b86069a534eec9429361ced0fbfd7335
Eli Almario <ee.almario@gmail.com>
2020-12-26 11:38:32 +0800

    Add restart upon gameOver

--- js/game.js
+++ js/game.js
@@ -15,7 +15,10 @@ let gameOver = false;
 
 function main (currentTime) {
   if (gameOver) {
-    return alert('You lost!');
+    if (confirm('You lost. Press ok to restart')) {
+      window.location = '/'
+    }
+    return 
   }
   
   window.requestAnimationFrame(main);
