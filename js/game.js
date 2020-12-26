17aee044db4944039967efd2033e4bdedfeffa1d
Eli Almario <ee.almario@gmail.com>
2020-12-26 11:33:08 +0800

    Add collision detection with wall and body

--- js/game.js
+++ js/game.js
@@ -1,12 +1,23 @@
-import { update as updateSnake, draw as drawSnake } from './snake.js';
+import { 
+  update as updateSnake, 
+  draw as drawSnake,
+  getSnakeHead,
+  snakeIntersection
+} from './snake.js';
 import { update as updateFood, draw as drawFood } from './food.js';
+import { outsideGrid } from './grid.js';
 import { SNAKE_SPEED } from './constants.js';
 
 // GAME LOOP
 const gameBoard = document.getElementById('gameBoard');
 let deltaTime = 0;
+let gameOver = false;
 
 function main (currentTime) {
+  if (gameOver) {
+    return alert('You lost!');
+  }
+  
   window.requestAnimationFrame(main);
   const secondsSinceLastRender = (currentTime - deltaTime) / 1000;
   
@@ -23,10 +34,15 @@ window.requestAnimationFrame(main);
 function update() {
   updateSnake();
   updateFood();
+  checkDeath();
 }
 
 function draw() {
   gameBoard.innerHTML = '';
   drawSnake(gameBoard);
   drawFood(gameBoard);
+}
+
+function checkDeath() {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
 }
