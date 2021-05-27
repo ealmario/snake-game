7f0a82cb90b2256723d6ee2ddb48c7eb89ed2b4a
ealmario <ee.almario@gmail.com>
2021-05-27 21:14:43 +0800

    Updates

--- js/game.js
+++ js/game.js
@@ -18,12 +18,12 @@ const loginBtn = document.getElementById('login');
 const backBtn = document.getElementById('back');
 const yesBtn = document.getElementById('yes');
 const noBtn = document.getElementById('no');
-// const switchBtn = document.getElementById('switch');
+const switchBtn = document.getElementById('switch');
 const instModal = document.getElementById('instModal');
 const titleModal = document.getElementById('titleModal');
 const overlayModal = document.getElementById('overlayModal');
 const gameOverModal = document.getElementById('gameOverModal');
-// const mobileContainer = document.getElementById('mobileContainer');
+const mobileContainer = document.getElementById('mobileContainer');
 const positionText = document.getElementById('position');
 
 // GAME LOOP
@@ -140,4 +140,4 @@ instBtn.addEventListener('click', instructions);
 backBtn.addEventListener('click', back);
 yesBtn.addEventListener('click', restartGame);
 noBtn.addEventListener('click', restartGame);
-// switchBtn.addEventListener('click', toggleSwitch);
+switchBtn.addEventListener('click', toggleSwitch);
